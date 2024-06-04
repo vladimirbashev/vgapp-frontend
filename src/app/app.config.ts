45a36f05408c151108991554163721e4477dafec
Volodymyr <vladimirbashev@gmail.com>
2024-06-04 19:55:32 +0200

    store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,19 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
+import {provideState, provideStore} from '@ngrx/store';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {metaReducers, reducers} from "./reducers";
+import {provideEffects} from "@ngrx/effects";
+import { provideRouterStore } from '@ngrx/router-store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore(), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideStore(reducers, { runtimeChecks: {}, metaReducers }),
+    provideState(messagesFeature),
+    provideEffects([ShopListEffects, BasketEffects]),
+    provideAnimationsAsync(),
+    provideRouterStore()
+]
 };
