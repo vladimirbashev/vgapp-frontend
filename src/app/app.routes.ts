010730009a74b191232605e1eb92864c84cef1f5
Volodymyr <vladimirbashev@gmail.com>
2024-06-06 17:06:52 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 
 export const routes: Routes = [
+  // {
+  //   path: 'auth',
+  //   loadChildren: () => import('./auth/auth.routes').then(r => r.routes),
+  // },
   {
     path: 'register',
     loadComponent: () => import('./auth/components/register/register.component').then(mod => mod.RegisterComponent)
