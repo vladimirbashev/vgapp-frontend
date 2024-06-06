56ec1fd528b790623e6b1b205a129935276666dc
Volodymyr <vladimirbashev@gmail.com>
2024-06-06 14:09:47 +0200

    auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: 'register',
+    loadComponent: () => import('./auth/components/register/register.component').then(mod => mod.RegisterComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./auth/components/login/login.component').then(mod => mod.LoginComponent)
+  }
+];
