d6414fed257a901c656e209a20a3f849c3551137
Volodymyr <vladimirbashev@gmail.com>
2024-07-04 16:56:54 +0200

    error

--- src/app/shared/components/error-message/error-message.component.ts
+++ src/app/shared/components/error-message/error-message.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
 import {AsyncPipe} from "@angular/common";
+import {ErrorInterface} from "../../types/error.interface";
 
 @Component({
   selector: 'app-error-message',
@@ -13,13 +14,22 @@ import {AsyncPipe} from "@angular/common";
   styleUrl: './error-message.component.scss'
 })
 export class ErrorMessageComponent implements OnInit{
-  @Input('value') value: Observable<any>
+  @Input('value') value: Observable<ErrorInterface | string>
   message: Observable<string>
 
   ngOnInit(): void {
     this.message = this.value.pipe(
       map((value) => {
-        return value && typeof value !== 'string' ? 'Internal error' : value
+        if (!value) {
+          return value;
+        }
+        if (typeof value === 'string') {
+          return value;
+        }
+        if (value['detail'] && typeof value['detail'] === 'string') {
+          return value['detail'];
+        }
+        return 'Internal error';
       })
     )
   }
---
+++ src/app/shared/types/error.interface.ts
@@ -0,0 +1,1 @@
+export interface ErrorInterface { [key: string]: any[] }
